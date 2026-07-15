9cc32bf94d01fa0f061a92109ce88b5483079d5b
Aweb <awebsolo23@gmail.com>
2026-07-15 11:25:00 +0100

    Lesson4

--- App.js
+++ App.js
@@ -2,15 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
+app.use(express.urlencoded({extended:true }))
+const Mydata = require("./models/myschema")
 
 app.get('/', (req,res) =>{
     res.sendFile("./Views/Home.html", {root: __dirname})
 })
 
-mongoose.connect("mongodb://awebsolo23_db_user:zOWM9B8u4c9GQGOP@ac-amenl6c-shard-00-00.76o1uhu.mongodb.net:27017,ac-amenl6c-shard-00-01.76o1uhu.mongodb.net:27017,ac-amenl6c-shard-00-02.76o1uhu.mongodb.net:27017/?ssl=true&replicaSet=atlas-h1r9j2-shard-0&authSource=admin&appName=Cluster0")
+mongoose.connect("mongodb://awebsolo23_db_user:zOWM9B8u4c9GQGOP@ac-amenl6c-shard-00-00.76o1uhu.mongodb.net:27017,ac-amenl6c-shard-00-01.76o1uhu.mongodb.net:27017,ac-amenl6c-shard-00-02.76o1uhu.mongodb.net:27017/all-data?ssl=true&replicaSet=atlas-h1r9j2-shard-0&authSource=admin&appName=Cluster0")
 .then( () => {
     app.listen(port, () =>{
     console.log(`http://localhost:${port}/`)
 })
 })
 .catch( (err) => {console.log(err)});
+
+app.post('/', (req,res) =>{
+   console.log(req.body)
+   const mydata = new Mydata(req.body)
+   mydata.save().then(() =>{
+        res.redirect("/")
+   }).catch((error) => {
+    console.log(error)
+   })
+
+
+})
